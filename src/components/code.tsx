35f7017a2e1c05a67b5ff41e36e29a767c60d792
rinz <roberts.denisovs@gmail.com>
2024-09-26 00:04:36 +0300

    another type fix

--- src/components/code.tsx
+++ src/components/code.tsx
@@ -1,9 +1,9 @@
 import SyntaxHighlighter from "react-syntax-highlighter";
 
-export default function Code({children}: any) {
-    return(
+export default function Code({ children }: { children: React.ReactNode }) {
+    return (
         <SyntaxHighlighter wrapLines showLineNumbers codeTagProps={{ className: 'text-shadow-none' }} customStyle={{ background: 'none', textShadow: 'none', color: 'white' }} >
-        {children}
-    </SyntaxHighlighter>
+            {children?.toString() || ""}
+        </SyntaxHighlighter>
     )
 }
